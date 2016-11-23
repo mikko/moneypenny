f71aa85a3cd757b389eaf0d6106bc06f4dbdcf93
Riku Rouvila <riku.rouvila@gmail.com>
2016-11-23 19:07:40 +0200

    add http server for receiving text-to-speech messages through http

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const speak = require('./src/speak');
 const fmi = require('./src/content/fmi');
 const smallTalk = require('./src/content/smalltalk');
@@ -6,9 +7,21 @@ const yleNews = require('./src/content/yleNews');
 const scheduler = require('./src/scheduler');
 const place = 'tampere';
 
+const app = express();
 
 speak.init()
     .then((say) => {
+
+      app.get('/say', (req, res) => {
+        if(!req.query.text) {
+          res.status(400).end();
+          return;
+        }
+        say(req.query.text);
+        res.status(200).end();
+      });
+
+      app.listen(process.env.PORT || 3000);
       yleNews.getText()
             .then(text => say(text));
         // scheduler.add('* * * * * *', () => say(smallTalk.getText()));
