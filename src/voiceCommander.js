bb3264db24157dfdcf3c2f5bdaa0753ee3e9e318
Riku Rouvila <riku.rouvila@gmail.com>
2016-11-25 13:57:50 +0200

    return promises from all commands

--- src/voiceCommander.js
+++ src/voiceCommander.js
@@ -13,62 +13,56 @@ const availableCommands = [
   {
     name: 'feedback',
     validate: command => ['feedback', 'palaute'].indexOf(command) !== -1,
-    execute: () => {
-      say('Please tell');
-      listen()
-        .then((results) => {
-          fb.registerFeedback(results);
-          say('Thank you! Coffee is important but feedback is importarter');
-        })
-        .catch(e => console.log('Something wrong listening to feedback', e));
-    },
+    execute: () =>
+      say('Please tell')
+      .then(listen)
+      .then((results) => {
+        fb.registerFeedback(results);
+        return say('Thank you! Coffee is important but feedback is importarter');
+      }).catch(e => console.log('Something wrong listening to feedback', e)),
   },
   {
     name: 'status',
     validate: command => ['status', 'mitä kuuluu'].indexOf(command) !== -1,
     execute: () => {
       const allFeedback = fb.getText();
-      say(`All feedback this week ${allFeedback}`)
+      return say(`All feedback this week ${allFeedback}`);
     },
   },
   {
     name: 'weather',
     validate: command => ['sää', 'weather', 'sataako', 'is it raining'].indexOf(command) !== -1,
-    execute: () => {
+    execute: () =>
       fmi.getText()
         .then(text => say(text))
-        .catch(e => say(e.message));
-    },
+        .catch(e => say(e.message)),
   },
   {
     name: 'news',
     validate: command => ['uutiset', 'uutisia', 'news'].indexOf(command) !== -1,
-    execute: () => {
+    execute: () =>
       yleNews.getText()
         .then(text => say(text))
-        .catch(e => say(e.message));
-    },
+        .catch(e => say(e.message)),
   },
   {
     name: 'bus',
     validate: command => ['antin bussi', 'bus', 'bussi', 'home'].indexOf(command) !== -1,
-    execute: () => {
+    execute: () =>
       tkl.getText()
         .then(text => say(`Only antti goes by bus. ${text}`))
-        .catch(e => say(e.message));
-    },
+        .catch(e => say(e.message)),
   },
 ];
 
 const execute = (word) => {
   const command = _.find(availableCommands, cmd => cmd.validate(word));
   if (!command) {
-    smallTalk.getText()
+    return smallTalk.getText()
       .then(text => say(text))
       .catch(e => say(e.message));
-  } else {
-    command.execute();
   }
+  return command.execute();
 };
 
 module.exports = {
