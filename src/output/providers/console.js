52f719a65a372b3170d1f72471648f263c741de1
Riku Rouvila <riku.rouvila@gmail.com>
2016-11-25 12:49:12 +0200

    fix console output

--- src/output/providers/console.js
+++ src/output/providers/console.js
@@ -1,8 +1,10 @@
 const Promise = require('bluebird');
 const chalk = require('chalk');
 
-const init = () => Promise.resolve(msg =>
-  console.log(`${chalk.gray('[slightly uncanny robotic voice says]:')} ${msg}`));
+const init = () => Promise.resolve((msg) => {
+  console.log(`${chalk.gray('[slightly uncanny robotic voice says]:')} ${msg}`);
+  return Promise.resolve();
+});
 
 module.exports = {
   init,
